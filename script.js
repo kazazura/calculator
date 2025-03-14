fa824c3ebe0ffbf93e469af70907515f3d31cd9d
kazazura <kazazura@gmail.com>
2025-03-14 16:40:03 +0800

    Create functions and variables for mathematical operations

---
+++ script.js
@@ -0,0 +1,35 @@
+let num1, operator, num2;
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
+// operator = '/';
+// num1 = 2;
+// num2 = 3;
+// console.log(operate(operator, num1, num2));

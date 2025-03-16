4ed738232d4ccdf417372de93120e2bc65e7bce5
kazazura <kazazura@gmail.com>
2025-03-16 12:00:45 +0800

    Add calculate function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let num1, operator, num2;
+let numbers = '1234567890';
+let num1, operator, num2, displayValue, result = 0;
 const display = document.querySelector('.display');
 const btnClear = document.querySelector('.clear');
 const btnNum = document.querySelectorAll('.number');
@@ -9,7 +10,7 @@ function add(num1, num2) {
     return num1 + num2;
 }
 
-function subtract(num1, num2){
+function subtract(num1, num2) {
     return num1 - num2;
 }
 
@@ -21,17 +22,69 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+function modulo(num1, num2) {
+    return num1 % num2;
+}
+
 function operate(operator, num1, num2) {
-    switch(operator) {
+    switch (operator) {
         case '+':
             return add(num1, num2);
         case '-':
             return subtract(num1, num2);
-        case '*':
+        case '×':
             return multiply(num1, num2);
-        case '/':
+        case '÷':
             return divide(num1, num2);
+        case '%':
+            return modulo(num1, num2);
     }
+}
+
+function calculate() {
+    let operandArr = [];
+    displayValue = display.value;
+    if (displayValue.includes('+')) {
+        operator = '+';
+        operandArr = displayValue.split('+');
+        num1 = operandArr[0];
+        num2 = operandArr[1];
+    } else if (displayValue.includes('÷')) {
+        operator = '÷';
+        operandArr = displayValue.split('÷');
+        num1 = operandArr[0];
+        num2 = operandArr[1];
+    } else if (displayValue.includes('×')) {
+        operator = '×';
+        operandArr = displayValue.split('×');
+        num1 = operandArr[0];
+        num2 = operandArr[1];
+    } else if (displayValue.includes('%')) {
+        operator = '%';
+        operandArr = displayValue.split('%');
+        num1 = operandArr[0];
+        num2 = operandArr[1];
+    } else if (displayValue.at(0) === '-' && displayValue.includes('-')) {
+        operator = '-';
+        operandArr = displayValue.split('-');
+        operandArr.shift();
+        num1 = operator + operandArr[0];
+        num2 = operandArr[1];
+    } else {
+        operator = '-';
+        operandArr = displayValue.split('-');
+        num1 = operandArr[0];
+        num2 = operandArr[1];
+    }
+
+    if((num1 === '' || num2 === '' || operator === '')) {
+        null;
+    } else {
+        result = operate(operator, Number(num1), Number(num2));
+        console.log("num1: " + num1, '\n' + "num2: " + num2, '\n' + "result: " + result);
+        display.value = result;
+    }
+   
 }
 
 function clearDisplay() {
@@ -45,24 +98,26 @@ function addToDisplay(input) {
 btnClear.addEventListener('click', clearDisplay);
 
 btnNum.forEach(btn => {
-    btn.addEventListener('click', ()=> {
-        addToDisplay(btn.value)
+    btn.addEventListener('click', () => {
+        addToDisplay(btn.value);
     })
 });
 
 btnOperator.forEach(btn => {
-    btn.addEventListener('click', ()=> {
-        addToDisplay(btn.value)
-    })
+    btn.addEventListener('click', () => {
+        if (display.value === '' && btn.value != '-') {
+            null;
+        } else if (display.value === '-') {
+            null;
+        } else {
+            addToDisplay(btn.value);
+        }
+    });
 });
 
-
-
-
-
-
-
-
+btnEquals.addEventListener('click', () => {
+    calculate();
+});
 
 
 // const display = document.querySelector('input');
